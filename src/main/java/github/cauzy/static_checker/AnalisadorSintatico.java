ef61ef92d3886192a511426e8bc49f6110e70e22
CauZy-Goes <cauafariasdev@gmail.com>
2025-06-07 14:55:54 -0300

    Inicialized Project

---
+++ src/main/java/github/cauzy/static_checker/AnalisadorSintatico.java
@@ -0,0 +1,8 @@
+package github.cauzy.static_checker;
+
+public class AnalisadorSintatico {
+
+    public static void main(String[] args) {
+        System.out.println("Analisador de Sintatico");
+    }
+}
